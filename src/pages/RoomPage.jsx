964830c9fd465768e961085df02483fc37e9d759
hoon <dotofi@naver.com>
2024-07-29 15:55:55 +0900

    [#15] ✨feat : RoomLayout 구현

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1,20 +1,15 @@
+// src/pages/RoomPage.jsx
 import React from 'react';
-import SidebarComponent from '../components/SidebarComponent';
-import HeaderNavComponent from '../components/HeaderNavComponent';
-import MainContentComponent from '../components/MainContentComponent';
+import RoomLayout from '../components/RoomLayout';
 import styles from '../styles/RoomPage.module.css';
 
 function RoomPage() {
     return (
-        <div className={styles.roomPage}>
-            <div className={styles.roomContent}>
-                <SidebarComponent />
-                <div className={styles.mainArea}>
-                    <HeaderNavComponent />
-                    <MainContentComponent />
-                </div>
+        <RoomLayout>
+            <div className={styles.roomPageContent}>
+                {/* RoomPage의 고유한 내용 */}
             </div>
-        </div>
+        </RoomLayout>
     );
 }
 
