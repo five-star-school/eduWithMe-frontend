29fb3057b8e2ccde156ceb8fec40a99539f77e85
jiwoo <1114jiwoo@gmail.com>
2024-08-02 17:01:31 +0900

    [#155] 🚚 move : HomePage 이동

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import MainPage from './pages/MainPage';
@@ -17,45 +17,34 @@ import ManageModifyPage from './pages/ManageModifyPage';
 import { AuthProvider } from './util/AuthContext';
 import ForgotPassword from './components/ForgotPassword';
 import MyRoomPage from './pages/MyRoomPage';
-
-function HomePage() {
-  return (
-      <div>
-        <h1>Home Page</h1>
-        <p>
-          <Link to="/main">메인 페이지로 이동</Link>
-        </p>
-        <p>여기에 웹페이지 소개랑 이용방법 적어도 좋을 것 같아요.</p>
-      </div>
-  );
-}
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-      <AuthProvider>
-        <Router>
-          <div className="App">
-            <Header />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/main" element={<MainPage />} />
-              <Route path="/room/:roomId" element={<RoomMainPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/changepassword" element={<ChangePasswordPage />} />
-              <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
-              <Route path="/room/:roomId/question/:questionId" element={<QuestionDetailPage/>}/>
-              <Route path="/room/:roomId/manageMain" element={<ManageMainPage/>}/>
-              <Route path="/manageCreate" element={<ManageCreatePage/>}/>
-              <Route path="/mypage" element={<MyPage />} />
-              <Route path="/room/:roomId/question/:questionId/manageRead" element={<ManageReadPage/>}/>
-              <Route path="/room/:roomId/question/:questionId/manageModify" element={<ManageModifyPage/>}/>
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/myroom" element={<MyRoomPage />} />
-            </Routes>
-          </div>
-        </Router>
-      </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/room/:roomId" element={<RoomMainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/changepassword" element={<ChangePasswordPage />} />
+            <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
+            <Route path="/room/:roomId/question/:questionId" element={<QuestionDetailPage />} />
+            <Route path="/room/:roomId/manageMain" element={<ManageMainPage />} />
+            <Route path="/manageCreate" element={<ManageCreatePage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/room/:roomId/question/:questionId/manageRead" element={<ManageReadPage />} />
+            <Route path="/room/:roomId/question/:questionId/manageModify" element={<ManageModifyPage />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/myroom" element={<MyRoomPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
